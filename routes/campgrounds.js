b482dd7b2667fc0bda24089b5db0b9c32245264f
Vikas Yadav <vikyadav20000@gmail.com>
2024-11-18 01:35:51 +0530

    Image Upload using cloudinary and multer (careful with the versions). use your cloudinary API

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,10 +5,24 @@ const ExpressError=require('../utils/ExpressError')
 const Campground = require('../models/campground');
 const {isLoggedIn,isAuthor,validateCamground}=require("../middleware.js");
 const campground=require("../controllers/campgrounds.js")
+const multer  = require('multer')
+const {storage}=require('../cloudinary')
+const upload = multer({storage});
 
 router.route('/')
         .get(catchAsync(campground.index))
-        .post(isLoggedIn,validateCamground,campground.createCampground)
+        // .post(isLoggedIn,validateCamground,campground.createCampground)
+        .post(upload.array('image'),(req,res)=>{
+                try{
+                        console.log(req.body,req.files)
+                        res.send("it worked");
+                }catch(err){
+                        console.error("File upload failed:", err);
+                        res.status(500).send("File upload failed");
+                }
+
+
+        })
 
 router.get('/new',isLoggedIn,campground.newCampgroundForm)
 
