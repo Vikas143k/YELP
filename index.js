51862fff798e78224380267172afdb5e414e7e29
Vikas Yadav <vikyadav20000@gmail.com>
2025-01-21 00:11:06 +0530

    Included sanitize-html to prevent XSS and included express-mongo-sanitize to prevent SQL Injection

--- index.js
+++ index.js
@@ -47,14 +47,14 @@ const sessionConfig={
 }
 app.use(session(sessionConfig))
 app.use(flash());
-
+const mongoSanitize = require('express-mongo-sanitize');
 
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
+app.use(mongoSanitize());
 app.use((req,res,next)=>{
     res.locals.currentUser=req.user;
     res.locals.success=req.flash("success");
